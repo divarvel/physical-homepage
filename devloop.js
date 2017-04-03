f01326de0b64a0124081d5d5c7b0421492855675
Clement Delafargue <clement@delafargue.name>
2017-04-03 23:16:50 +0200

    Fix devloop server reloading

--- devloop.js
+++ devloop.js
@@ -9,6 +9,6 @@ let server = runServer({
   httpPort,
   env: { "PORT": httpPort },
   sh: `./.stack-work/install/x86_64-osx/lts-8.2/8.0.2/bin/spock-exe`
-})
+}).dependsOn(compile)
 
 proxy(server, 8080).dependsOn(compile)
