1db78753231d4e21d65da3347d759ab5e97fc620
Clement Delafargue <clement.delafargue@fretlink.com>
2020-05-15 17:33:41 +0200

    Make target paragraphs more visible

--- app/Views/MainPage.hs
+++ app/Views/MainPage.hs
@@ -24,8 +24,8 @@ mainPage talks = mainLayout $ do
 
 talkBlock :: Talk -> H.Html
 talkBlock talk =
-  H.div ! A.class_ "section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone" $ do
-    H.h5 ! A.id (H.toValue slug) $
+  H.div ! A.class_ "section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone" ! A.id (H.toValue slug) $ do
+    H.h5 $
       H.a ! A.class_ "permalink" ! A.href (H.toValue $ "#" <> slug) $
         H.toHtml $ title talk
     H.p $ H.toHtml $ description talk
